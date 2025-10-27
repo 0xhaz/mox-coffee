06abb3d51f7174cd45ad3778f9c1f8eb74c265b9
0xhaz <hazrienne@gmail.com>
2025-10-27 19:58:21 +0800

    Updated deploy

--- script/deploy.py
+++ script/deploy.py
@@ -8,9 +8,12 @@ from src import buy_me_a_coffee
 
 def moccasin_main():
     active_network = get_active_network()
-    price_feed: VyperContract = deploy_feed()
-    coffee = buy_me_a_coffee.deploy(price_feed.address)
-    print(f"Deployed Coffee contract at {coffee.address} with price feed {price_feed.address} on {active_network.name}")
+    price_feed: VyperContract = active_network.manifest_named("price_feed")
+    print(f"On network {active_network.name}, found price feed at {price_feed.address}")
+    return price_feed
+    # price_feed: VyperContract = deploy_feed()
+    # coffee = buy_me_a_coffee.deploy(price_feed.address)
+    # print(f"Deployed Coffee contract at {coffee.address} with price feed {price_feed.address} on {active_network.name}")
 
-    print(coffee.get_eth_to_usd_rate(1000))
+    # print(coffee.get_eth_to_usd_rate(1000))
     
